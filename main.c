846ca8c7896254ca46ad81a96b3eb526580e2c8e
ekf0129 <153363067+ekf0129@users.noreply.github.com>
2024-11-04 01:30:45 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "game.h"
+#include <iostream>
+#include <cstdlib>  // rand(), srand()
+#include <ctime>    // time()
+
+int main() {
+	init();
+        }
+
+        int main() {
+            // 게임 시작 메시지 출력
+            std::cout << "게임을 시작합니다!" << std::endl;
+
+            // girlfriend() 함수 호출
+            girlfriend();
+
+            // 프로그램 종료
+            return 0;
+        }
+	}
+	return 0;
+
+
+	
+}
