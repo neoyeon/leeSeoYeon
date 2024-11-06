a5730bcfa940a2a9951ee899424957f63ddebb47
LeeSeoYeon <dkdlqhdn@gmail.com>
2024-11-06 16:22:05 +0900

    898989

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef __STDIO_H__ 
+#define __STDIO_H__
+
+#include <stdio.h>
+
+#endif
+
+
+#ifndef __WINDOWS_H__ 
+#define __WINDOWS_H__
+
+#include <windows.h> //gotoxy, sleep 를 갖고 있음
+
+#endif
+
